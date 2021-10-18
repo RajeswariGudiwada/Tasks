3d0ef37fb73b0d58bd7cd6ed3fb0e72a99bcd966
RajeswariGudiwada <92358572+RajeswariGudiwada@users.noreply.github.com>
2021-10-18 19:37:20 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,56 +6,59 @@ namespace sails
 {
     class Bank
     {
-        public Bank(string name) //constructor
+        public Bank(string name)
         {
             marks = new List<double>();
-            this.name = name;
+
         }
-
-
         public void AddMark(double mark)
         {
             marks.Add(mark);
         }
-       public List<double> marks;
-        private string name;
+        public void showMarks()
+        {
+            int x = 10;
+            int y = 11;
+            int result = x + y;
+            Console.WriteLine($"Addition is {result}");
+
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            var total = 2.2;
+            var highMark = double.MinValue;
+            var lowMark = double.MaxValue;
+
+            foreach (var no in numbers)
+            {
+                lowMark = Math.Min(no, lowMark);
+                highMark = Math.Max(no, highMark);
+                total += no;
+            }
+            Console.WriteLine($"lowest grade is {lowMark}");
+            Console.WriteLine($"highest grade is {highMark}");
+            Console.WriteLine($"Total is {total:N1}");
+
+            List<double> marks = new List<double>() { 12.7, 10.3, 6.11, };
+            marks.Add(6.4);
+            marks.Add(7.9);
+            Console.WriteLine(marks.Count);
+
+        }
+        public List<double> marks;
+
     }
     class Program
     {
         static void Main(string[] args)
         {
             var bank = new Bank("hello");
-            bank.AddMark(67.5);
-            bank.marks.Add(101);
-
-            int x = 10;
-            int y = 11;
-            int result = x + y;
-            Console.WriteLine($"Addition is {result}"); //addition
-
-            var numbers = new [] {12.7,10.3,6.11,4.1};
-            var total = 2.2;
-            foreach( var no in numbers)
-            {
-                total += no;
-            }
-            Console.WriteLine($"Total is {total:N1}");//Array
-
-            List<double> marks = new List<double>() { 12.7, 10.3, 6.11,};
-            marks.Add(6.4);
-            marks.Add(7.9);
-            Console.WriteLine(marks.Count);//List
-
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"{args[0]} {args[1]}!");
-            }
-            else
-            {
-                Console.WriteLine("welcome!");
-            }
+            bank.AddMark(82.5);
+            bank.AddMark(23.5);
+            bank.AddMark(6.5);
+            bank.showMarks();
         }
     }
 }
 
+    
 
+   
