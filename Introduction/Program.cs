8954f1646f1fb514ef0d975d08dc77420d7d6fb0
RajeswariGudiwada <92358572+RajeswariGudiwada@users.noreply.github.com>
2021-10-25 20:42:45 +0530

    Add files via upload

---
+++ Introduction/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            IEnumerable<India> cities = new List<India>()
+            {
+                new  India{ Id=101, Name = "chennai" },
+                new  India{ Id=102, Name = "mumbai" },
+                new  India{ Id=102, Name = "hyderabad" },
+                new  India{ Id=102, Name = "Delhi" },
+                new  India{ Id=102, Name = "bangalore" }
+            };
+
+            IEnumerable<India> villages = new List<India>()
+            {
+                new  India{ Id=201, Name = "Diskit" },
+                 new  India{ Id=201, Name = "Landour" },
+                 new  India{ Id=201, Name = "Malana" },
+                 new  India{ Id=201, Name = "Nako" },
+                 new  India{ Id=201, Name = "Majuli" }
+            };
+
+            Console.WriteLine("no of cities = {0}", cities.Count());
+            Console.WriteLine("***cities are***");
+            foreach (var i in cities)
+            {
+                Console.WriteLine(i.Name);
+
+            }
+            foreach (var india in cities.Where(
+              c => c.Name.StartsWith("D")
+              ))
+            {
+                Console.WriteLine("***city name starts with D is***");
+                Console.WriteLine(india.Name);
+
+            }
+
+            Console.WriteLine("no of villages = {0}", villages.Count());
+            Console.WriteLine("***villages are***");
+            foreach (var i in villages)
+            {
+                Console.WriteLine(i.Name);
+
+            }
+            foreach (var india in villages.Where(
+              c => c.Name.StartsWith("D")
+              ))
+            {
+                Console.WriteLine("***city name starts with D is***");
+                Console.WriteLine(india.Name);
+
+            }
+        }
+
+
+    }
+
+
+}
+
---
+++ Introduction/features.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Introduction1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\SAILS-DM150";
+            showLargeFilesWithoutLinq(path);
+            Console.WriteLine("***");
+            showLargeFilesWithLinq(path);
+        }
+
+        private static void showLargeFilesWithLinq(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(7);
+            foreach (var file in query.Take(9))
+            {
+                Console.WriteLine($"{file.Name,-20}:{file.Length,10:N0}");
+            }
+        }
+
+        private static void showLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+            for (int i = 0; i < 8; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name,-20}:{file.Length,10:N0}");
+            }
+
+
+        }
+    }
+
+}
+
+public class FileInfoComparer : IComparer<FileInfo>
+{
+    public int Compare(FileInfo x, FileInfo y)
+    {
+        return y.Length.CompareTo(x.Length);
+    }
+
+}
