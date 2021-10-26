aab559bb5f2a48ffc5f64e724bc857be38bd8458
RajeswariGudiwada <92358572+RajeswariGudiwada@users.noreply.github.com>
2021-10-26 19:49:14 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,64 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
-
-namespace sails
+namespace Introduction
 {
-    class Bank
-    {
-        public Bank(string name)
-        {
-            marks = new List<double>();
-
-        }
-        public void AddMark(double mark)
-        {
-            marks.Add(mark);
-        }
-        public void showMarks()
-        {
-            int x = 10;
-            int y = 11;
-            int result = x + y;
-            Console.WriteLine($"Addition is {result}");
-
-            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
-            var total = 2.2;
-            var highMark = double.MinValue;
-            var lowMark = double.MaxValue;
-
-            foreach (var no in numbers)
-            {
-                lowMark = Math.Min(no, lowMark);
-                highMark = Math.Max(no, highMark);
-                total += no;
-            }
-            Console.WriteLine($"lowest grade is {lowMark}");
-            Console.WriteLine($"highest grade is {highMark}");
-            Console.WriteLine($"Total is {total:N1}");
-
-            List<double> marks = new List<double>() { 12.7, 10.3, 6.11, };
-            marks.Add(6.4);
-            marks.Add(7.9);
-            Console.WriteLine(marks.Count);
-
-        }
-        public List<double> marks;
-
-    }
     class Program
     {
-        static void Main(string[] args)
+    
+        public static void Main(string[] args)
         {
-            var bank = new Bank("hello");
-            bank.AddMark(82.5);
-            bank.AddMark(23.5);
-            bank.AddMark(6.5);
-            bank.showMarks();
+            IEnumerable<India> cities = new List<India>()
+            {
+                new  India{ Id=101, Name = "chennai" },
+                new  India{ Id=102, Name = "mumbai" },
+                new  India{ Id=103, Name = "hyderabad" },
+                new  India{ Id=104, Name = "delhi" },
+                new  India{ Id=105, Name = "bangalore" }
+            };
+
+            IEnumerable<India> villages = new List<India>()
+            {
+                 new  India{ Id=201, Name = "Diskit" },
+                 new  India{ Id=202, Name = "Landour" },
+                 new  India{ Id=203, Name = "Malana" },
+                 new  India{ Id=204, Name = "Nako" },
+                 new  India{ Id=205, Name = "Majuli" }
+            };
+           
+            Console.WriteLine("***Number of cities in List = {0}", cities.Count());
+            Console.WriteLine("Cities are:");
+            foreach (var c in cities)
+            {
+                Console.WriteLine(c.Name);
+
+            }
+            Console.WriteLine("\n***order by length=9:");
+            foreach (var c in cities.Where(e=>e.Name.Length==9)
+                                     .OrderByDescending(e=>e.Name))
+            {
+
+                Console.WriteLine(c.Name);
+
+            }
+            foreach (var c in cities.Where(
+              c => c.Name.StartsWith("b")
+              ))
+            {
+                Console.WriteLine("\n***City name starts with b is:");
+                Console.WriteLine(c.Name);
+
+            }
+
+            Console.WriteLine("\n***Number of villages = {0}", villages.Count());
+            Console.WriteLine("villages are:");
+            foreach (var v in villages)
+            {
+                Console.WriteLine(v.Name);
+
+            }
+            var query = from village in villages
+                        where village.Name.Length == 4
+                        orderby village.Name ascending
+                        select village;
+            foreach( var i in query)
+            {
+                Console.WriteLine("\n***Order by length=4:");
+                Console.WriteLine( i.Name);
+            }
+            foreach (var india in villages.Where(
+              c => c.Name.StartsWith("N")
+              ))
+            {
+                Console.WriteLine("\n***City name starts with N is:");
+                Console.WriteLine(india.Name);
+
+            }
+            Console.WriteLine("\n*** Usingfunc");
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) => x + y;
+            Console.WriteLine(square(add(5, 2))); 
+            Console.WriteLine("\n");
+            Console.WriteLine("***Using filter:");
+            var query1 = cities.Filter(m => m.Id > 102);
+           
+            foreach (var p in query1)
+            {
+
+                Console.WriteLine(p.Name);
+            }
+            Console.WriteLine("\n**Using Where:");
+            var query2 = cities.Where(m => m.Id > 102);
+         
+            foreach (var p in query1)
+            {
+
+                Console.WriteLine(p.Name);
+            }
         }
+
+
     }
+
+
 }
 
-    
-
-   
