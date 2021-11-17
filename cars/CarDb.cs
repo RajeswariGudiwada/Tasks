1498ea4829c972e46d92c4887169cbdf7f57aa7e
RajeswariGudiwada <gudiwadarajee@gmail.com>
2021-11-17 11:55:44 +0530

    first commit

---
+++ cars/CarDb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Cars
+{
+    class CarDb : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
